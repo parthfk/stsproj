fb3a85ee018edc8ea99542269630765eab5f562c
parthfk <parth.aggarwal@flipkart.com>
2023-01-24 14:30:12 +0530

    First commit

---
+++ src/com/flipkart/app/DemoClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.app;
+
+/**
+ * @author parth.aggarwal
+ *
+ */
+public class DemoClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
